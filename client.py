c000bdd6941c75b74122f8ecc16ba0beb1a7f890
Raymond Chastain <rlc5m8@pc3.instageni.northwestern.edu>
2019-11-12 11:06:42 -0800

    Initial commit of quic capable services

--- client.py
+++ client.py
@@ -1,47 +1,109 @@
-import requests
 import math
 import cv2
 import numpy as np
 import time
+from http3_client import HttpClient, save_session_ticket
 
-frameCount = 1
-peakFPS = 0
-minFPS = 10000
-averageFPS =0
-FPS = []
-startTime = time.time()
-video = cv2.VideoCapture("Road traffic video for object recognition.mp4")
+from aioquic.asyncio.client import connect
+from aioquic.h3.connection import H3_ALPN
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.h3.events import DataReceived, H3Event, HeadersReceived
+from typing import cast
+import asyncio
+import argparse
+import sys
+from urllib.parse import urlparse
 
 def getNextServer():
-	file = open("NextServer.txt", "r")
-	return file.read()[:-1]
+    file = open("NextServer.txt", "r")
+    return file.read()[:-1]
 
-uri = "http://" + getNextServer() + "/frameProcessing"
-#force 640x480 webcam resolution
-video.set(3,640)
-video.set(4,480)
+def save_session_ticket(ticket):
+    """
+    Callback which is invoked by the TLS engine when a new session ticket
+    is received.
+    """
+    logger.info("New session ticket received")
+    with open('session_ticket', "wb") as fp:
+        pickle.dump(ticket, fp)
 
-for i in range(0,20):
-    (grabbed, frame) = video.read()
+async def run(args):
+    frameCount = 1
+    peakFPS = 0
+    minFPS = 10000
+    averageFPS =0
+    FPS = []
+    startTime = time.time()
+    video = cv2.VideoCapture("Road traffic video for object recognition.mp4")
 
-while True:
-	frameStartTime = time.time()
-	frameCount+=1
-	(grabbed, frame) = video.read()
-	height = np.size(frame,0)
-	width = np.size(frame,1)
+    uri = "http://" + getNextServer() + "/frameProcessing"
+    parsed = urlparse(uri)
+    host, port_str = parsed.netloc.split(":")
+    port = int(port_str)
+    #force 640x480 webcam resolution
+    video.set(3,640)
+    video.set(4,480)
 
-    #if cannot grab a frame, this program ends here.
-	if not grabbed:
-		break
-	#cv2.imwrite("Frame.jpg", frame)
-	#print(frame.shape)
-	data = {"Frame":frame.tolist()}
-	r = requests.post(uri, json = data)
-	currentFPS = 1.0/(time.time() - frameStartTime)
-	FPS.append(currentFPS)
-	print("response = {}, frame = {}, fps = {} ".format(r, frameCount, round(currentFPS, 3) ))
-	if r == "<Response [500]>":
-		break
-print("Average FPS = {}".format(round(np.mean(FPS), 3)))
-print("RunTimeInSeconds = {}".format(round(frameStartTime - startTime, 3)))
+    for i in range(0,20):
+        (grabbed, frame) = video.read()
+    configuration = QuicConfiguration(
+        is_client=True, alpn_protocols=H3_ALPN
+    )
+    configuration.load_verify_locations(args.ca_certs)
+    async with connect(
+            host,
+            port,
+            configuration=configuration,
+            create_protocol=HttpClient,
+            session_ticket_handler=save_session_ticket,
+        ) as client:
+            client = cast(HttpClient, client)
+            while True:
+                frameStartTime = time.time()
+                frameCount+=1
+                (grabbed, frame) = video.read()
+                height = np.size(frame,0)
+                width = np.size(frame,1)
+                #if cannot grab a frame, this program ends here.
+                if not grabbed:
+                    break
+                #cv2.imwrite("Frame.jpg", frame)
+                #print(frame.shape)
+                data = {"Frame":frame.tolist()}
+
+                http_events = await client.post(
+                    u"/frameProcessing",
+                    data=str(data).encode('utf-8'),
+                )
+                currentFPS = 1.0/(time.time() - frameStartTime)
+                FPS.append(currentFPS)
+                for http_event in http_events:
+                    if isinstance(http_event, HeadersReceived):
+                        headers = b""
+                        for k, v in http_event.headers:
+                            headers += k + b": " + v + b"\r\n"
+                        if headers:
+                            sys.stderr.buffer.write(headers + b"\r\n")
+                            sys.stderr.buffer.flush()
+                        print("frame = {}, fps = {} ".format(frameCount, round(currentFPS, 3) ))
+
+            print("Average FPS = {}".format(round(np.mean(FPS), 3)))
+            print("RunTimeInSeconds = {}".format(round(frameStartTime - startTime, 3)))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="HTTP/3 client")
+    parser.add_argument(
+        "--ca-certs", type=str, help="load CA certificates from the specified file"
+    )
+    parser.add_argument(
+        "-s",
+        "--session-ticket",
+        type=str,
+        help="read and write session ticket from the specified file",
+    )
+    args = parser.parse_args()
+    asyncio.run(run(args))
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(
+    #     run(args)
+    # )
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, url_for, send_file, Response, jsonify
-from flask_restful import Resource, Api
+# from flask import Flask, request, url_for, send_file, Response, jsonify
+# from flask_restful import Resource, Api
+from quart import Quart, request, url_for, send_file, Response, jsonify
+
 import cv2
 import requests
 import os
@@ -9,6 +11,18 @@ import numpy as np
 #import time
 import json
 from collections import OrderedDict
+import asyncio
+
+# quic
+from http3_client import HttpClient, save_session_ticket
+from aioquic.asyncio.client import connect
+from aioquic.h3.connection import H3_ALPN
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.h3.events import DataReceived, H3Event, HeadersReceived
+from typing import cast
+import argparse
+import sys
+from urllib.parse import urlparse
 
 #global variables
 width = 0
@@ -20,6 +34,7 @@ binarizationThreshold = 30  #Adjust ths value to tweak the binarization
 offsetEntranceLine = 30  #offset of the entrance line above the center of the image
 offsetExitLine = 60
 yolodir = "./YOLO"
+certdir = './aioquic/tests'
 outputFile = "output.txt"
 referenceFrame = 0
 dilatedFrame = 0
@@ -30,202 +45,225 @@ LABELS = open(labelsPath).read().strip().split("\n")
 weightsPath = os.path.sep.join([yolodir, "yolov3.weights"])
 configPath = os.path.sep.join([yolodir, "yolov3.cfg"])
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+certs = os.path.sep.join([certdir, "pycacert.pem"])
 
-
-app = Flask(__name__)
-api = Api(app)
-
+app = Quart(__name__)
 
 @app.route("/")
 def index():
-	return "Image processing functional decomposition"
-	
+    return "Image processing functional decomposition"
+
 @app.route("/init", methods = ["POST"])
 def init():
-	"""
-	Re-initialize the object counter to 0 for all objects.
-	"""
-	print("Initializing Application")
-	labelDict = {}
-	labelsPath = os.path.sep.join([yolodir, "coco.names"])
-	LABELS = open(labelsPath).read().strip().split("\n")
-	for label in LABELS:
-		labelDict[label] = 0
-	json.dump(labelDict, open(outputFile, "w"))	
-	return "Output Counter Initialized"
+    """
+    Re-initialize the object counter to 0 for all objects.
+    """
+    print("Initializing Application")
+    labelDict = {}
+    labelsPath = os.path.sep.join([yolodir, "coco.names"])
+    LABELS = open(labelsPath).read().strip().split("\n")
+    for label in LABELS:
+        labelDict[label] = 0
+    json.dump(labelDict, open(outputFile, "w"))
+    return "Output Counter Initialized"
+
+async def json_from_byte_array(bytes):
+    return json.loads(bytes.decode(encoding='utf-8').replace("'", '"'))
 
 @app.route("/frameProcessing", methods = ["POST"])
-def frameProcessing():
-	"""
-	Receive the frames from a video sequentially and count the number of objects. Each object is sent to a classifier which saves the count of objects that have been seen.
-	"""
-	global referenceFrame
-	global dilatedFrame
-	#receive the image from the request.
-	file = request.json
-	frame = np.array(file["Frame"], dtype = "uint8")
-	
-	#gray-scale conversion and Gaussian blur filter applying
-	grayFrame = greyScaleConversion(frame)
-	blurredFrame = gaussianBlurring(grayFrame)
+async def frameProcessing():
+    """
+    Receive the frames from a video sequentially and count the number of objects. Each object is sent to a classifier which saves the count of objects that have been seen.
+    """
+    global referenceFrame
+    global dilatedFrame
+    #receive the image from the request
+    data = await request.get_data()
+    file = await json_from_byte_array(data)
+    frame = np.array(file["Frame"], dtype = "uint8")
+    configuration = QuicConfiguration(
+        is_client=True, alpn_protocols=H3_ALPN
+    )
+    configuration.load_verify_locations(certs)
+    #gray-scale conversion and Gaussian blur filter applying
+    grayFrame = greyScaleConversion(frame)
+    blurredFrame = gaussianBlurring(grayFrame)
 
-	#Check if a frame has been previously processed and set it as the previous frame.
-	if type(referenceFrame) ==int():
-		referenceFrame = blurredFrame
-	
-	#Background subtraction and image binarization
-	frameDelta = getImageDiff(referenceFrame, blurredFrame)
-	referenceFrame = blurredFrame
-	#cv2.imwrite("previousImage.jpg", blurredFrame)
-	frameThresh = thresholdImage(frameDelta, binarizationThreshold)
+    #Check if a frame has been previously processed and set it as the previous frame.
+    if type(referenceFrame) ==int():
+        referenceFrame = blurredFrame
 
-	#Dilate image and find all the contours
-	dilatedFrame = dilateImage(frameThresh)
-	#cv2.imwrite("dilatedFrame.jpg", dilatedFrame)
-	cnts = getContours(dilatedFrame.copy())
-	
-	height = np.size(frame,0)
-	coordYEntranceLine = int((height / 2)-offsetEntranceLine)
-	coordYExitLine = int((height / 2)+offsetExitLine)
-	headers = {"enctype" : "multipart/form-data"}
-	r = requests.post("http://" + getNextServer() + "/objectClassifier", headers = headers, json = {"Frame":frame.tolist()} )
-	"""	
-	for c in cnts:
-		print("x")
-		if cv2.contourArea(c) < minContourArea:
-			print("Small Area", cv2.contourArea(c))
-			continue
-		(x, y, w, h) = getContourBound(c)
-		#grab an area 2 times larger than the contour.
-		cntImage  = frame[y:y+int(1.5*w), x:x+int(1.5*h)]
-		objectCentroid = getContourCentroid(x, y, w, h)
-		coordYCentroid = (y+y+h)/2
-		
-		
-		#if (checkEntranceLineCrossing(coordYCentroid,coordYEntranceLine,coordYExitLine)):				
-		headers = {"enctype" : "multipart/form-data"}
-		#i = random.randint(1,1000)
-		#cv2.imwrite("ContourImages/contour"+str(i)+".jpg", cntImage)
-		#files = {"image":open("ContourImages/contour"+str(i)+".jpg", "rb")}
-		data = {"contour" : cntImage.tolist()}
-		r = requests.post("http://" + getNextServer() + "/objectClassifier", headers = headers, json = data )
+    #Background subtraction and image binarization
+    frameDelta = getImageDiff(referenceFrame, blurredFrame)
+    referenceFrame = blurredFrame
+    #cv2.imwrite("previousImage.jpg", blurredFrame)
+    frameThresh = thresholdImage(frameDelta, binarizationThreshold)
 
-	
-	"""
-	return Response(status=200)
+    #Dilate image and find all the contours
+    dilatedFrame = dilateImage(frameThresh)
+    #cv2.imwrite("dilatedFrame.jpg", dilatedFrame)
+    cnts = getContours(dilatedFrame.copy())
+
+    height = np.size(frame,0)
+    coordYEntranceLine = int((height / 2)-offsetEntranceLine)
+    coordYExitLine = int((height / 2)+offsetExitLine)
+    headers = {"enctype" : "multipart/form-data"}
+    uri = "http://" + getNextServer() + "/objectClassifier"
+    parsed = urlparse(uri)
+    host, port_str = parsed.netloc.split(":")
+    port = int(port_str)
+    async with connect(
+            host,
+            port,
+            configuration=configuration,
+            create_protocol=HttpClient,
+            session_ticket_handler=save_session_ticket,
+        ) as client:
+            client = cast(HttpClient, client)
+            http_events = await client.post(
+                u"/objectClassifier",
+                data=str(json.dumps({"Frame":frame.tolist()})).encode('utf-8')
+            )
+
+    # r = requests.post(, headers = headers, json =  )
+    """
+    for c in cnts:
+        print("x")
+        if cv2.contourArea(c) < minContourArea:
+            print("Small Area", cv2.contourArea(c))
+            continue
+        (x, y, w, h) = getContourBound(c)
+        #grab an area 2 times larger than the contour.
+        cntImage  = frame[y:y+int(1.5*w), x:x+int(1.5*h)]
+        objectCentroid = getContourCentroid(x, y, w, h)
+        coordYCentroid = (y+y+h)/2
+
+
+        #if (checkEntranceLineCrossing(coordYCentroid,coordYEntranceLine,coordYExitLine)):
+        headers = {"enctype" : "multipart/form-data"}
+        #i = random.randint(1,1000)
+        #cv2.imwrite("ContourImages/contour"+str(i)+".jpg", cntImage)
+        #files = {"image":open("ContourImages/contour"+str(i)+".jpg", "rb")}
+        data = {"contour" : cntImage.tolist()}
+        r = requests.post("http://" + getNextServer() + "/objectClassifier", headers = headers, json = data )
+
+
+    """
+    return {"status": 200}
 
 @app.route("/objectClassifier", methods = ["POST"])
-def classifier():
-	"""
-	Classify an object and update the counter maintained at output.txt.
-	"""
-	print("Classifying")
-	#initialize important variables
-	minConfidence = 0.5
-	thresholdValue = 0.3
-	
-	"""
-	file = request.files#['image']
-	file.save("./classifier_image.jpg")
-	frame = cv2.imread("./classifier_image.jpg")
-	"""
-	file = request.json
-	frame = np.array(file["Frame"], dtype = "uint8") 
+async def classifier():
+    """
+    Classify an object and update the counter maintained at output.txt.
+    """
+    print("Classifying")
+    #initialize important variables
+    minConfidence = 0.5
+    thresholdValue = 0.3
 
-	#file = request.files['image']
-	#file.save("./classifier_image.jpg")
-	#frame = cv2.imread("./classifier_image.jpg")
-	#file = request.json
-	#frame = np.array(file["contour"], dtype="uint8")
-	
-	#Get Image dimensions
-	image = cv2.copyMakeBorder(frame, 30, 30, 30, 30, cv2.BORDER_CONSTANT, value=255)
-	(H, W) = image.shape[:2]
-	
-	#Get the output layers parameters
-	ln = net.getLayerNames()
-	ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-	
-	#Create a blob to do a forward pass
-	blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-	net.setInput(blob)
-	#print(H, W)
-	layerOutputs = net.forward(ln)
-	print(type(net))
-	boxes = []
-	confidences = []
-	classIDs = []
-	for output in layerOutputs:
-		print("detecting")
-		#loop over each detection
-		for detection in output:
-			# extract the class ID and confidence (i.e., probability) of
-			# the current object detection
-			scores = detection[5:]
-			classID = np.argmax(scores)
-			confidence = scores[classID]
+    """
+    file = request.files#['image']
+    file.save("./classifier_image.jpg")
+    frame = cv2.imread("./classifier_image.jpg")
+    """
+    data = await request.get_data()
+    file = await json_from_byte_array(data)
+    frame = np.array(file["Frame"], dtype = "uint8")
 
-			# filter out weak predictions by ensuring the detected
-			# probability is greater than the minimum probability
-			if confidence > minConfidence:
-				# scale the bounding box coordinates back relative to the
-				# size of the image, keeping in mind that YOLO actually
-				# returns the center (x, y)-coordinates of the bounding
-				# box followed by the boxes' width and height
-				box = detection[0:4] * np.array([W, H, W, H])
-				(centerX, centerY, width, height) = box.astype("int")
+    #file = request.files['image']
+    #file.save("./classifier_image.jpg")
+    #frame = cv2.imread("./classifier_image.jpg")
+    #file = request.json
+    #frame = np.array(file["contour"], dtype="uint8")
 
-				# use the center (x, y)-coordinates to derive the top and
-				# and left corner of the bounding box
-				x = int(centerX - (width / 2))
-				y = int(centerY - (height / 2))
+    #Get Image dimensions
+    image = cv2.copyMakeBorder(frame, 30, 30, 30, 30, cv2.BORDER_CONSTANT, value=255)
+    (H, W) = image.shape[:2]
 
-				# update our list of bounding box coordinates, confidences,
-				# and class IDs
-				boxes.append([x, y, int(width), int(height)])
-				confidences.append(float(confidence))
-				classIDs.append(classID)
+    #Get the output layers parameters
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-	# apply non-maxima suppression to suppress weak, overlapping bounding
-	# boxes
-	idxs = cv2.dnn.NMSBoxes(boxes, confidences, minConfidence, thresholdValue)
+    #Create a blob to do a forward pass
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    #print(H, W)
+    layerOutputs = net.forward(ln)
+    print(type(net))
+    boxes = []
+    confidences = []
+    classIDs = []
+    for output in layerOutputs:
+        print("detecting")
+        #loop over each detection
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability) of
+            # the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
 
-	# ensure at least one detection exists
-	if len(idxs) > 0:
-		output = json.load(open(outputFile))
-		# loop over the indexes we are keeping
-		for i in idxs.flatten():
-			# extract the bounding box coordinates
-			(x, y) = (boxes[i][0], boxes[i][1])
-			(w, h) = (boxes[i][2], boxes[i][3])
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > minConfidence:
+                # scale the bounding box coordinates back relative to the
+                # size of the image, keeping in mind that YOLO actually
+                # returns the center (x, y)-coordinates of the bounding
+                # box followed by the boxes' width and height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
 
-			print(LABELS[classIDs[i]], output[LABELS[classIDs[i]]]+1, confidences[i])
-			output[LABELS[classIDs[i]]]+=1
-		
-		json.dump(output, open(outputFile, "w"))
-		return LABELS[classIDs[i]]
-	else:
-		return Response(status=200)
+                # use the center (x, y)-coordinates to derive the top and
+                # and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                # update our list of bounding box coordinates, confidences,
+                # and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    # apply non-maxima suppression to suppress weak, overlapping bounding
+    # boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, minConfidence, thresholdValue)
+
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        output = json.load(open(outputFile))
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            print(LABELS[classIDs[i]], output[LABELS[classIDs[i]]]+1, confidences[i])
+            output[LABELS[classIDs[i]]]+=1
+
+        json.dump(output, open(outputFile, "w"))
+        return {"label": LABELS[classIDs[i]], "status": 200}
+    else:
+        return {"status": 200}
 
 
 @app.route("/getCounts", methods = ["GET"])
 def getCounts():
-	retval ={}
-	output = json.load(open(outputFile))
-	for key, val in output.items():
-		if val >=1:
-			retval[key] = val
-	return jsonify(retval)
+    retval ={}
+    output = json.load(open(outputFile))
+    for key, val in output.items():
+        if val >=1:
+            retval[key] = val
+    return jsonify(retval)
 
 @app.route("/setNextServer", methods = ["POST"])
 def setNextServer():
-	json.dump(request.json, open("NextServer.txt", "w"))
-	
-def getNextServer():
-	file = open("NextServer.txt", "r")
-	return file.read()[:-1]
+    json.dump(request.json, open("NextServer.txt", "w"))
 
-	
+def getNextServer():
+    file = open("NextServer.txt", "r")
+    return file.read()[:-1]
+
+
 def getContourCentroid(x, y, w, h):
     """
     Get the centroid/center of the countours you have
@@ -235,7 +273,7 @@ def getContourCentroid(x, y, w, h):
     coordYCentroid = (y+y+h)/2
     objectCentroid = (int(coordXCentroid),int(coordYCentroid))
     return objectCentroid
-	
+
 #Check if an object in entering in monitored zone
 def checkEntranceLineCrossing(y, coorYEntranceLine, coorYExitLine):
     absDistance = abs(y - coorYEntranceLine)
@@ -244,10 +282,10 @@ def checkEntranceLineCrossing(y, coorYEntranceLine, coorYExitLine):
         return 1
     else:
         return 0
-	
+
 def getContours(frame):
     """
-    Get the contours in the frame 
+    Get the contours in the frame
     @return: contours
     """
     contours, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -291,6 +329,3 @@ def dilateImage(frame, interations=2 ):
     Dilate the image to prevent spots that are black inside an image from being counted as individual objects
     """
     return cv2.dilate(frame, None, iterations=2)
-
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", debug=True)
